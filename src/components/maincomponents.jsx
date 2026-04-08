3a5e8f437e7b1e135b4b8cf64aec0d14a5225135
Amulya Sharma <amulyasharma0987@gmail.com>
2026-04-08 23:27:40 +0530

    update UI / fix bug

--- src/components/maincomponents.jsx
+++ src/components/maincomponents.jsx
@@ -10,7 +10,7 @@ export default function Maincomponents(){
         sethide(!hide)
     }
     return(
-        <div className="w-[94%] flex items-center max-h-[1080px] min-h-[73%] bg-[#EBEBEB] rounded-[23px] ml-1">
+        <div className="w-[94%] flex items-start max-h-[1080px] min-h-[73%] bg-[#EBEBEB] rounded-[23px] ml-1">
             <Mostleftticket value={show}/>
             <Leftticket value={show} functionn={setshow} hide={hide} sethide={sethide}/>
             {hide?<></>:<span className="relative z-10 h-screen min-w-[2px] hover:bg-blue-600" onClick={(e)=>{e.stopPropagation();handleclick()}}></span>}
