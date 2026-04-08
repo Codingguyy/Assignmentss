06a51b5622ddae455562cb062299f3e8ad08c9a0
Amulya Sharma <amulyasharma0987@gmail.com>
2026-04-09 01:15:34 +0530

    Fix bugs and UI

--- src/components/prioritybutton.jsx
+++ src/components/prioritybutton.jsx
@@ -12,7 +12,7 @@ export default function PriorityButton(){
         <span className="mr-1 text-sm text-white 2xl:text-base">To Do</span>
         <span className="mx-2 h-full w-px bg-gray-300"></span>
         <ChevronDown size={17} className="mt-1 text-white" />
-        {value?<div className="absolute w-[324px] h-[213px] top-[93%] left-[0%] border border-[1px] border border-gray rounded-md bg-white">{array.map(data=><div className="h-[71px] flex flex-col items-start">
+        {value?<div className="absolute w-[324px] h-[213px] top-[93%] left-[0%] border border-[1px] border border-gray rounded-md bg-white">{array.map(data=><div className="h-[71px] flex flex-col items-start hover:bg-[#F2F2F2]">
             <span className="px-2 flex items-center justify-start gap-3">
                 <MoveRight size={37} color="black"/>
                 <span className="text-medium text-primary">{data.title}</span>
